7af3f7f963bfdb7a891f886a317a358069b548e8
plirapli <rafliyulistiawan@gmail.com>
2024-05-05 19:30:51 +0700

    add http request for add and edit

--- lib/util/todo.dart
+++ lib/util/todo.dart
@@ -14,6 +14,32 @@ class TodoApi {
     return jsonDecode(response.body)["data"];
   }
 
+  static Future<Map<String, dynamic>> addTodo(String title, String isi) async {
+    final http.Response response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{'title': title, 'isi': isi}),
+    );
+    return jsonDecode(response.body);
+  }
+
+  static Future<Map<String, dynamic>> editTodo(
+    String id,
+    String title,
+    String isi,
+  ) async {
+    final http.Response response = await http.put(
+      Uri.parse(url + id),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{'title': title, 'isi': isi}),
+    );
+    return jsonDecode(response.body);
+  }
+
   static Future<Map<String, dynamic>> deleteTodo(String id) async {
     final http.Response response = await http.delete(Uri.parse(url + id));
     return jsonDecode(response.body);
